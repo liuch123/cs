c511b2ab39aa50d94ed8899ad1484198c64cad44
huilei123 <test@3129261190.com>
2023-06-11 16:54:19 +0800

    gloal

---
+++ src/main/java/edu/czjt/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+/*ass="container">
+<div
+class="tableBar"
+        style="display: inline-block"
+        >
+<el-button
+        type="primary"
+class="continue"
+@click="addClass('class')"
+        >新增菜品分类
+        + 新增菜品分类
+</el-button>
+<el-button
+        type="primary"
+@click="addClass('meal')"
+        >
+        + 新增套餐分类
+</el-button>
+</div>
+<el-table
+        :data="tableData"
+        stripe
+class="tableBox"
+        >
+<el-table-column
+        prop="name"
+        label="分类名称"
+        />
+*/
